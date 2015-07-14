986645dace4a94f5a317b6625b6278eeba9295ae
呂紹淳 <oslo0322@gmail.com>
2015-07-14 21:10:05 +0800

    update

---
+++ ichef_deploy/recipes/default.rb
@@ -0,0 +1,14 @@
+bash 'install_something' do
+  user 'ec2-user'
+  cwd '/home/ec2-user/ichef2/'
+  code <<-EOH
+  sudo su ec2-user
+  eval "$(ssh-agent -s)"
+  ssh-add /home/ec2-user/.ssh/github_rsa
+  git pull
+  EOH
+end
+
+service "uwsgi" do
+  action :restart
+end
--- recipes/default.rb
+++
@@ -1,6 +0,0 @@
-bash 'install_something' do
-  code <<-EOH
-  mkdir bagayalo
-  git clone git@github.com:iCHEF/aws_opsworks.git bushit
-  EOH
-end
