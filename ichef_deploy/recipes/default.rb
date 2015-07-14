710c690346b1abba553d9ca8878886d159379ae8
呂紹淳 <oslo0322@gmail.com>
2015-07-14 20:31:58 +0800

    no message

--- ichef_deploy/recipes/default.rb
+++ ichef_deploy/recipes/default.rb
@@ -3,6 +3,8 @@ bash 'install_something' do
   cwd '/home/ec2-user/ichef2/'
   code <<-EOH
   sudo su ec2-user
+  eval "$(ssh-agent -s)"
+  ssh-add /home/ec2-user/.ssh/github_rsa
   git pull
   EOH
 end
